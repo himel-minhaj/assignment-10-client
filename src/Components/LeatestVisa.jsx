fd9311beb1e9557d8c3e96a91bd31f546c1db419
himel-minhaj <himelminhajul@gmail.com>
2025-02-11 00:40:49 +0600

    add leatest visa in home page

---
+++ src/Components/LeatestVisa.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
+import { Cursor, Typewriter, useTypewriter } from "react-simple-typewriter";
+
+const LeatestVisa = () => {
+  const visaData = useLoaderData();
+  // console.log(allVisa);
+  const [allVisa, setAllVisa] = useState(visaData);
+
+  return (
+    <div className="dark:text-white">
+      <h1 className="md:text-4xl font-bold text-center mt-3 ">
+        Apply for visa for{" "}
+        <span className="font-bold text-red-600">
+          <Typewriter
+            words={[
+              "destinations worldwide",
+              "Explore World Wide",
+              "Adventure Trails",
+              "Hop-on",
+              "The Royal Route",
+            ]}
+            loop={5}
+            // cursor
+            // cursorStyle=">"
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+            // onLoopDone={handleDone}
+            // onType={handleType}
+          />
+        </span>
+        <span>
+          <Cursor cursorColor="green" cursorStyle="/" />
+        </span>
+      </h1>
+      <p className="text-lg text-center my-2 w-9/12 mx-auto">
+        The Visa Navigator helps you to quickly and easily find the visa
+        you,tailored for your travel, study, or work needs. Learn about visa
+        categories, fees, processing times, and more in one seamless platform
+        need
+      </p>
+
+      <div className="md:grid md:grid-cols-3 p-2 mx-auto ">
+        {allVisa.slice(0, 6).map((visa) => (
+          <div
+            key={visa._id}
+            className="card bg-base-100 my-3 md:w-80 shadow-2xl"
+          >
+            <figure className="  border ">
+              <img
+                className=" object-cover w-full md:h-[200px]"
+                src={visa.countryImageUrl}
+                alt=""
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                {visa.countryName}
+                {/* <div className="badge badge-secondary">NEW</div> */}
+              </h2>
+              <div>
+                <p>Processing_time :{visa.Processing_time} </p>
+                <p>Validity : {visa.Processing_time}</p>
+              </div>
+              <div className="flex justify-center items-center gap-2 ">
+                <h1 className=" text-red-600 font-bold text-xl">
+                  Fee :${visa.fee}
+                </h1>
+                <Link to={`/Details/${visa._id}`} className="btn btn-success">
+                  Details
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-center items-center">
+        <Link to={"/Allvisas"} className="btn bg-orange-400 p-4">
+         See All Visa
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default LeatestVisa;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "../Components/Banner";
 import Feedback from "../Components/Feedback";
 import AboutUs from "../Components/AboutUs";
+import LeatestVisa from "../Components/LeatestVisa";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <section className="my-5 mt-32 md:mt-20">
         <Banner></Banner>
       </section>
+     <LeatestVisa></LeatestVisa>
       <AboutUs></AboutUs>
       <Feedback></Feedback>
     </div>
